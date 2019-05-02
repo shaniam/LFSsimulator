5608307e8222a9b3a18980b84a772b86609b3e9e
Scott Mason <scottmason@Macbook-Pro-4.stny.rr.com>
2019-05-02 04:25:18 -0400

    going crazy here

--- main.cpp
+++ main.cpp
@@ -21,17 +21,17 @@ int inmem_index=0;
 int segindex=KILO;
 vector<int> ssbnodes={};
 
-vector<char> openBlockInSegment(KILO * KILO);
+char openBlockInSegment[KILO * KILO];
 int openBlock = 0;
 int segNum = 0;
-vector<vector<int>> summary(KILO, vector<int>(2));
-vector<int> checkpoint(40);
-vector<int> imap(40 * KILO);
+int summary[KILO][2];
+int checkpoint[40];
+int imap[40 * KILO];
 
 void hardDrive(){
-  mkdir("DRIVE", 0777);
+	mkdir("DRIVE", 0777);
 
-  int y = 0;
+	int y = 0;
 	string name = "DRIVE/SEGMENT";
 	string checkpoint = "DRIVE/CHECKPOINT_REGION";
 	string fileNameMap = "DRIVE/FILENAMEMAP";
@@ -42,7 +42,7 @@ void hardDrive(){
 		string iter=to_string(i);
 		string file=name+iter+".txt";
 		ofstream outfile(file, ios::binary);
-		
+		char y = 0;
 
 		if(!outfile.is_open()){
 			cerr << "Problem with segment " << i << endl;
@@ -51,7 +51,7 @@ void hardDrive(){
 
 		//1048576 / 4
 		for (int x = 0; x < 1048576; x++){
-		  outfile.write(reinterpret_cast<const char*>(&y), sizeof(char));
+			outfile.write(reinterpret_cast<const char*>(&y), sizeof(y));
 		}
 
 		outfile.close();
@@ -62,7 +62,7 @@ void hardDrive(){
 		exit(-1);
 	}
 
-	for (int x = 0; x < 1048576; x++){
+	for (int x = 0; x < 1048576 / 4; x++){
 		outfile2.write(reinterpret_cast<const char*>(&y), sizeof(y));
 	}
 
@@ -89,6 +89,17 @@ void import(string file, string lfsFile){
   fileIn.seekg(0, ios::end);
   int fileSize = fileIn.tellg();
   fileIn.seekg(0, ios::beg);
+
+	if ((fileSize / KILO) > KILO - openBlock){
+		fstream seg("DRIVE/SEGMENT"+std::to_string(segNum)+".txt", ios::binary);
+
+		seg.write(openBlockInSegment, KILO * KILO);
+		seg.write(reinterpret_cast<const char*>(&summary), 8192);
+
+		seg.close();
+
+		openBlock = 0;
+}
 
 	//cout << "File size: " << fileSize << endl;
 
@@ -132,9 +143,9 @@ void import(string file, string lfsFile){
 	fileNameMap.seekp(iNodeNum * 128);
 	fileNameMap.write(lfsCArray, (n + (128 - n)));
 
-	vector<char> inputFileBuffer(fileSize);
-	fileIn.read(inputFileBuffer.data(), fileSize);
-	memcpy(&openBlockInSegment.at(openBlock * KILO), inputFileBuffer.data(), fileSize);
+	char inputFileBuffer[fileSize];
+	fileIn.read(inputFileBuffer, fileSize);
+	memcpy(&openBlockInSegment[openBlock * KILO], inputFileBuffer, fileSize);
 
 	// for(int i = 0; i < inputFileBuffer.size(); i++){
 	// 	cout << inputFileBuffer[i];
@@ -148,9 +159,9 @@ void import(string file, string lfsFile){
 	inode.fileName = lfsFile;
 	inode.size = fileSize;
 	for (int i = 0; i <= fileSize / KILO; i++){
-    inode.dataBlock.at(i) = openBlock + (segNum - 1) * KILO;
-    summary.at(openBlock).at(0) = iNodeNum;
-    summary.at(openBlock).at(1) = i;
+    inode.dataBlock[i] = openBlock + (segNum - 1) * KILO;
+    summary[openBlock][0] = iNodeNum;
+    summary[openBlock][1] = i;
     openBlock++;
 		//cout << i << endl;
   }
@@ -162,37 +173,75 @@ void import(string file, string lfsFile){
 	// 	cout << inode.dataBlock[i];
 	// }
 
-	summary.at(openBlock).at(0) = iNodeNum;
-	summary.at(openBlock).at(1) = -1;
+	summary[openBlock][0] = iNodeNum;
+	summary[openBlock][1] = -1;
 
-	memcpy(&openBlockInSegment.at(openBlock * KILO), &inode, sizeof(inode));
+	memcpy(&openBlockInSegment[openBlock * KILO], &inode, sizeof(inode));
 	openBlock++;
 
 	if (openBlock == KILO){
-		fstream seg("DRIVE/SEGMENT"+std::to_string(segNum)+".txt", fstream::binary);
+		fstream seg("DRIVE/SEGMENT"+to_string(segNum)+".txt", ios::binary);
 
-		seg.write(openBlockInSegment.data(), KILO * KILO);
+		seg.write(openBlockInSegment, KILO * KILO);
 		seg.write(reinterpret_cast<const char*>(&summary), 8192);
 
 		seg.close();
 		cout << "run" << endl;
 	}
 
-  imap.at(iNodeNum) = (openBlock - 1) + (segNum * KILO);
+  imap[iNodeNum] = (openBlock - 1) + (segNum * KILO);
 
-  memcpy(&openBlockInSegment.at(openBlock * KILO), &imap.at(frag * (KILO / 4)), KILO);
+  memcpy(&openBlockInSegment[openBlock * KILO], &imap[frag * (KILO / 4)], KILO);
 
-  summary.at(openBlock).at(0) = -1;
-  summary.at(openBlock).at(1) = frag;
+  summary[openBlock][0] = -1;
+  summary[openBlock][1] = frag;
 
-  checkpoint.at(frag) = openBlock + segNum * KILO;
+  checkpoint[frag] = openBlock + segNum * KILO;
 
   openBlock++;
 
+	// fstream in("DRIVE/SEGMENT0.txt", fstream::binary);
+	//
+	// char buffer[fileSize];
+	// in.read(buffer, fileSize);
+	//
+	// for(int i = 0; i < fileSize; i++){
+	// 	cout << buffer[i];
+	// }
+
+	fileIn.close();
+}
+
+void shutdown() {
+	fstream seg("DRIVE/SEGMENT"+std::to_string(segNum)+".txt", fstream::binary );
+
+  seg.write(openBlockInSegment, 1024 * 1024);
+  seg.write(reinterpret_cast<const char*>(&summary), 8192);
+
+  seg.close();
+
+  openBlock = 0;
+
+	std::fstream check("DRIVE/CHECKPOINT_REGION.txt", fstream::binary);
+
+  char checkBuf[160];
+  memcpy(checkBuf, checkpoint, 160);
+  check.write(checkBuf, 160);
+
+  check.close();
 }
 int main(){
 	hardDrive();
 
 	import("other.txt", "DRIVE/SEGMENT0.txt");
+	shutdown();
+
+	char buffer[10];
+	ifstream inFile("DRIVE/SEGMENT0.txt", ios::binary);
+	inFile.read(buffer, 1);
+
+	for(int i = 0; i < 10; i++){
+		cout << buffer[i] << endl;
+	}
 
 }
